164ee2565caceee38926580523a233e833a61a96
Arindom <arindom956@gmail.com>
2025-10-28 15:55:32 +0530

    minor fixes

--- portfolio-website/src/components/Navbar/navbar.tsx
+++ portfolio-website/src/components/Navbar/navbar.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
 import styles from "../Navbar/navbar.module.scss"
 
 const Navbar = () => {
   return (
     <div>
-        <div className={styles.navbarparent}>
-           <div id={styles.nameid}>Arindom</div>
-           <div className={styles.sectionclass}>
+      <div className={styles.navbarparent}>
+        <div id={styles.nameid}>Arindom</div>
+        <div className={styles.sectionclass}>
           <div>
             <a href='https://drive.google.com/file/d/1nif68TtGpijRuPW2QxUurTr2Zppl3hWx/view?usp=sharing' target='_blank'>
-            #my-resume</a>
+              #my-resume</a>
           </div>
-           </div>
         </div>
+      </div>
     </div>
   )
 }
