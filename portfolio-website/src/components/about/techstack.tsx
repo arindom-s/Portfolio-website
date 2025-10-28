69e5431a6fcb8858a78bfc4e6cbb5079a1509c66
Arindom <arindom956@gmail.com>
2025-10-28 16:34:43 +0530

    Icons modified

--- portfolio-website/src/components/about/techstack.tsx
+++ portfolio-website/src/components/about/techstack.tsx
@@ -1,20 +1,21 @@
 export const Techstack=[
     { name: "C++", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627831/3_ukipc2.png", docs:"https://www.geeksforgeeks.org/c-plus-plus/"},
     { name: "JavaScript", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627830/13_udjffd.png", docs:"https://developer.mozilla.org/en-US/docs/Web/JavaScript"},
-    { name: "TypeScript", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627830/icons8-typescript-50_jv7jql.png", docs:"https://www.typescriptlang.org/docs/"},
+    { name: "TypeScript", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761649285/icons8-typescript-50_1_lelhx9.png", docs:"https://www.typescriptlang.org/docs/"},
     { name: "HTML5", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627830/Programming_logos_and_icons_by_hrhasnai_5_wszrtk.png", docs:"https://developer.mozilla.org/en-US/docs/Glossary/HTML5"},
     { name: "CSS3", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627830/Programming_logos_and_icons_by_hrhasnai_9_hvn9cf.png", docs:"https://developer.mozilla.org/en-US/docs/Web/CSS"},
-    { name: "MySQL", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627831/Programming_logos_and_icons_by_hrhasnai_31_avextt.png", docs:"https://downloads.mysql.com/docs/mysql-tutorial-excerpt-8.0-en.a4.pdf"},
+    { name: "SQL", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761648855/icons8-sql-50_mrp3vv.png", docs:"https://downloads.mysql.com/docs/mysql-tutorial-excerpt-8.0-en.a4.pdf"},
     { name: "React", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627831/Programming_logos_and_icons_by_hrhasnai_35_u1c8ud.png", docs:"https://react.dev"},
-    { name: "Express", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739628517/Express_wnf9dw.png", docs:"https://expressjs.com/"},
+    { name: "Express", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761648471/icons8-express-js-50_1_rui3yo.png", docs:"https://expressjs.com/"},
     { name: "Tailwind", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739628695/Tailwind_CSS_vcs0lp.png", docs:"https://tailwindcss.com/"},
     { name: "MongoDB", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739628517/MongoDB_dmp89y.png", docs:"https://www.mongodb.com/docs/"},
-    { name: "Prisma", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739628695/prisma-icon-423x512-isxs6996_gkuo73.png", docs:"https://www.prisma.io/docs"},
+    { name: "Prisma", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761648555/icons8-prisma-orm-50_stjoae.png", docs:"https://www.prisma.io/docs"},
     { name: "Git", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629093/Git_v2f4ym.png", docs:"https://git-scm.com/doc"},
-    { name: "GitHub", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629216/GitHub_q0i5eh.png", docs:"https://docs.github.com/en/get-started/start-your-journey/about-github-and-git"},
+    { name: "GitHub", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761649387/icons8-github-50_hjhoo8.png", docs:"https://docs.github.com/en/get-started/start-your-journey/about-github-and-git"},
     { name: "Node", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739627831/Programming_logos_and_icons_by_hrhasnai_15_uprgc0.png", docs: "https://nodejs.org/docs/latest/api/synopsis.html" },
     { name: "Redis", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629093/Redis_gvbyjd.png", docs:"https://redis.io/docs/latest/"},
-    { name: "Next", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629092/Next.js_ugl5hi.png", docs:"https://nextjs.org/docs"},
+    { name: "Next", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761649204/icons8-next.js-48_sxezxm.png", docs:"https://nextjs.org/docs"},
     { name: "Scss", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739628695/Sass_lscsst.png", docs:"https://sass-lang.com/documentation/"},
-    { name: "Nodemon", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629093/Nodemon_krdurs.png", docs:"https://www.npmjs.com/package/nodemon"}
+    { name: "Nodemon", type: "frameworks&tools", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1739629093/Nodemon_krdurs.png", docs:"https://www.npmjs.com/package/nodemon"},
+    { name: "Go", type: "language", imgSrc: "https://res.cloudinary.com/dl0knguhb/image/upload/v1761649119/icons8-golang-48_hmuado.png", docs:"https://go.dev/doc/"}
 ];
